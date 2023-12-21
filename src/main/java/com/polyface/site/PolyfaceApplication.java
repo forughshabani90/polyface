28934435c22849d20aa5f50d31ccbb98b286f085
forughshabani90 <forugh.shabani.90@gmail.com>
2023-12-21 06:08:21 -0800

    fc

---
+++ src/main/java/com/polyface/site/PolyfaceApplication.java
@@ -0,0 +1,13 @@
+package com.polyface.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polyface/site/PolyfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyface.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
